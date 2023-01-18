49f6c67c40587da4755f4955aff66ccb55632536
andrija-tosic <andonnet@gmail.com>
2023-01-18 18:39:56 +0100

    init api project

---
+++ API/CityPalAPI/Controllers/CityPalController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+
+namespace CityPalAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class CityPalController : ControllerBase
+{
+
+    private readonly ILogger<CityPalController> logger;
+    private readonly IBoltGraphClient graphClient;
+
+    public CityPalController(ILogger<CityPalController> logger, IBoltGraphClient graphClient)
+    {
+        this.logger = logger;
+        this.graphClient = graphClient;
+    }
+
+    [HttpGet("CityPalTest")]
+    public List<string> Get()
+    {
+        return new List<string>();
+    }
+}
---
+++ API/CityPalAPI/Program.cs
@@ -0,0 +1,39 @@
+using Neo4jClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IBoltGraphClient>(options =>
+{
+    var client = new BoltGraphClient(
+        new Uri(builder.Configuration.GetConnectionString("Neo4j")),
+        builder.Configuration.GetSection("Neo4j:User").Value,
+        builder.Configuration.GetSection("Neo4j:Password").Value
+    );
+
+    client.ConnectAsync().Wait();
+    return client;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
